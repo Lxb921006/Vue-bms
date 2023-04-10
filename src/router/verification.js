c61de8f83dc1614a13b5cb679ffda1036c1f4fc3
Lxb921006 <120332269@qq.com>
2023-04-10 18:30:40 +0800

    router.beforeEach的使用细节

--- src/router/verification.js
+++ src/router/verification.js
@@ -1,6 +1,7 @@
 import router from './index'
 import store from "../store/index"
 
+// 当if条件都成立时, next("/index")跳转到index页面, 此时的router.beforeEach并没有结束, 需要调用next()作为跳出出口,否则会发生死循环
 router.beforeEach((to, from, next) => {
   if (sessionStorage.getItem('token') && sessionStorage.getItem('user') && sessionStorage.getItem('uid')) {
     // 已登录,显示对应用户权限的页面
